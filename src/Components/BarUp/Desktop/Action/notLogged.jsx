85c39699337c783091ae686efac6a5db15f943ea
cadacis <73143818+cadacis@users.noreply.github.com>
2022-05-27 18:15:16 -0500

    V0.2
    Fix Bar

--- src/Components/BarUp/Desktop/Action/notLogged.jsx
+++ src/Components/BarUp/Desktop/Action/notLogged.jsx
@@ -5,14 +5,16 @@ import Button from '@mui/material/Button';
 import Badge from '@mui/material/Badge';
 import Dialog from '@mui/material/Dialog';
 import Typography from '@mui/material/Typography';
-
+import Menu from '@mui/material/Menu';
 const NotLogged = () => {
   const [loginDialog, setLoginDialog] = React.useState(false);
-
+  const [cart, setCart] = React.useState(false);
   const handleLoginDialog = () => {
     setLoginDialog(!loginDialog);
   };
-
+  const handleCart = () => {
+    setCart(!cart);
+  };
   return (
     <Box>
       <Dialog onClose={handleLoginDialog} open={loginDialog}>
@@ -21,10 +23,35 @@ const NotLogged = () => {
           Login Form Here
         </Typography>
       </Dialog>
+      <Menu
+        sx={{ mt: '45px' }}
+        id="menu-appbar"
+        /*  anchorEl={cart} */
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        open={cart}
+        onClose={handleCart}>
+        <Box>
+          {' '}
+          <Typography textAlign="center">Cart Commerce</Typography>
+        </Box>
+      </Menu>
+
       <Button sx={{ borderRadius: 12 }} variant="text" color="white">
         <Icon icon="carbon:search" color="white" width="35" height="35" />
       </Button>
-      <Button sx={{ borderRadius: 12 }} variant="text" color="white">
+      <Button
+        onClick={handleCart}
+        sx={{ borderRadius: 12 }}
+        variant="text"
+        color="white">
         <Badge color="secondary" badgeContent={0} showZero>
           <Icon icon="mdi-light:cart" color="white" width="35" height="35" />
         </Badge>
